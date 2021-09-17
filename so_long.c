8e33a874f0ac14c3424e9ff54c054ef4228ee9e0
Nicolas RUBIN <nrubin@e1r11p8.42.fr>
2021-09-17 13:08:08 +0200

    updated error messages

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: nrubin <nrubin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 11:59:24 by nrubin            #+#    #+#             */
-/*   Updated: 2021/09/17 13:04:43 by nrubin           ###   ########.fr       */
+/*   Updated: 2021/09/17 13:07:57 by nrubin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,14 +79,14 @@ int	main(int argc, char **argv)
 	t_map = map_init();
 	if (check_args(argc, argv) == -1)
 	{
-		printf("Error\n");
+		printf("Error\nArgument error.\n");
 		return (-1);
 	}
 	if (check_fd(argv) == 0)
 		fd = open(argv[1], O_RDONLY);
 	else
 	{
-		printf("Error\n");
+		printf("Error\nFile Descriptor error.\n");
 		return (-1);
 	}
 	get_map_height(fd, argv, &t_map);
@@ -95,7 +95,7 @@ int	main(int argc, char **argv)
 	read_map(fd, argv, &t_map);
 	close(fd);
 	if (check_map(t_map) == -1)
-		printf("Error\n");
+		printf("Error\nMap error.\n");
 	free_map(&t_map);
 	return (0);
 }
